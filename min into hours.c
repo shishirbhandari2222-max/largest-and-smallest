50a41fd66c56285d8436eb30b1051f3e11a2f61a
shishirbhandari2222-max <shishirbhandari2222@gmail.com>
2025-08-22 21:49:55 +0545

    minutes into hours

---
+++ min into hours.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main()
+{
+	/*convert min into hr*/ 
+	int a,c;
+	printf("enter minutes to be converted:");
+	scanf("%d",&a);
+	
+	c=a/60;
+	printf("The time in hour is:%d hr",c);	
+return 0;	
+}
