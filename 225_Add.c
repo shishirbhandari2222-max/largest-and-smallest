95b17a0298eb890b0a8f7984d6e1ea023cc354a0
shishirbhandari2222-max <shishirbhandari2222@gmail.com>
2025-08-23 00:23:40 +0545

    Add files via upload
    add two number

---
+++ 225_Add.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{ 
+    int num1, num2, result;
+    printf("enter first number: ");
+    scanf("%d",&num1);
+    
+    printf("enter second number: ");
+	scanf("%d",&num2);
+	
+	result = num1+num2;
+	printf("Result=%d\n",result);
+	return 0;
+
+}
+
+
+
+
+
+
